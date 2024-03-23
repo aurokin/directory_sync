4665d3c9199f5b7da8742627d3c6c03a9f789703
Hunter Sadler <auro@hsadler.com>
2024-03-23 03:58:39 -0600

    Run command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let mut custom_print = Command::new("echo");
+    custom_print.arg("meow");
+    let output = custom_print.output().expect("Error running echo");
+    let output_str = String::from_utf8(output.stdout).unwrap();
+    // custom_print("Twiggle says MEOW");
+    println!("{output_str}");
 }
