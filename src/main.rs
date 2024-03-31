0ac2593025eb2e5a42f9084a48eb69b243d3d219
Hunter Sadler <auro@hsadler.com>
2024-03-30 20:42:12 -0600

    Path Changes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::process::Command;
+use std::path::Path;
 
 fn main() {
-    let mut custom_print = Command::new("echo");
-    custom_print.arg("meow");
-    let output = custom_print.output().expect("Error running echo");
-    let output_str = String::from_utf8(output.stdout).unwrap();
-    // custom_print("Twiggle says MEOW");
-    println!("{output_str}");
+    println!("Hello World");
+    let path = Path::new("/Users/auro/.dirsync.toml");
+    let path_exists = path.exists();
+    let path_is_file = path.is_file();
+    println!("{path_exists}");
+    println!("{path_is_file}");
 }
