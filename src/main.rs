fa179dc147a2abce9551e23def07bb8e7ca860f0
Hunter Sadler <auro@hsadler.com>
2024-04-01 16:47:09 -0600

    Config

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
+use std::fs;
 use std::path::Path;
+
+// struct Folder {
+//     name: String,
+//     path: String,
+// }
 
 fn main() {
     println!("Hello World");
+    let config = read_config().expect("Error Reading File");
+    println!("{config}");
+}
+
+fn read_config() -> Option<String> {
     let path = Path::new("/Users/auro/.dirsync.toml");
     let path_exists = path.exists();
     let path_is_file = path.is_file();
-    println!("{path_exists}");
-    println!("{path_is_file}");
+
+    if path_exists && path_is_file {
+        let file = fs::read_to_string(path).expect("Error reading file");
+        return Some(file);
+    }
+
+    return None;
 }
