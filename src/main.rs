ba4027441ef53876df5ba40ded18d38e92c9178a
Hunter Sadler <auro@hsadler.com>
2024-04-01 19:48:57 -0600

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,24 @@ use std::path::Path;
 
 fn main() {
     let config = read_config().expect("Error reading config");
+    parse_config(config);
+}
+
+fn read_config() -> Option<String> {
+    let home_dir = home_dir().expect("No Home Dir");
+    let path = Path::new(&home_dir.as_os_str()).join(".dirsync.toml");
+    let path_exists = path.exists();
+    let path_is_file = path.is_file();
+
+    if path_exists && path_is_file {
+        let file = fs::read_to_string(path).expect("Error reading file");
+        return Some(file);
+    }
+
+    return None;
+}
+
+fn parse_config(config: String) -> () {
     let config: TomlConfig = toml::from_str(config.as_str()).expect("Error parsing config");
 
     let mut ssh_servers: HashMap<String, SshServer> = HashMap::new();
@@ -37,18 +55,4 @@ fn main() {
         let folder = folder_map.1;
         println!("{:?}: {:?}, {:?}", folder.name, folder.path, folder.target);
     }
-}
-
-fn read_config() -> Option<String> {
-    let home_dir = home_dir().expect("No Home Dir");
-    let path = Path::new(&home_dir.as_os_str()).join(".dirsync.toml");
-    let path_exists = path.exists();
-    let path_is_file = path.is_file();
-
-    if path_exists && path_is_file {
-        let file = fs::read_to_string(path).expect("Error reading file");
-        return Some(file);
-    }
-
-    return None;
 }
