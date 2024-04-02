62ccbc4c3290f082685cdfdc11387303aaa47845
Hunter Sadler <auro@hsadler.com>
2024-04-02 00:29:23 -0600

    Parse LS Command

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cargo run -- --cmd test
+cargo run -- --cmd ls 
--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,8 @@ fn main() {
     let config = read_config().expect("Error reading config");
     let (ssh_servers, folders) = parse_config(config);
     let args = Args::parse();
-
-    println!("{:#?}\n{:#?}\n{:#?}\n", args, ssh_servers, folders);
+    match args.cmd.as_str() {
+        "ls" => println!("MEOW - ls - MEOW"),
+        _ => println!("{:#?}\n{:#?}\n{:#?}\n", args, ssh_servers, folders),
+    }
 }
